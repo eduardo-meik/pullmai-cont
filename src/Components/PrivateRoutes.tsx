b648ca333f3d2fa7ec8d3e5768b5fec59138ca06
eduardo-meik <eduardo@meiklabs.com>
2025-06-16 16:44:45 -0400

    Refactor PrivateRoutes and implement App component with authentication logic

--- src/Components/PrivateRoutes.tsx
+++ src/Components/PrivateRoutes.tsx
@@ -1,13 +1,11 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
-import DashNavbar from './DashNavbar'
 
 export default function PrivateRoutes() {
   const { currentUser } = useAuth()
 
   return currentUser ? (
     <>
-      <DashNavbar />
       <Outlet />
     </>
   ) : (
